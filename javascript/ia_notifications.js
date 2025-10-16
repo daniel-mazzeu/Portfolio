3d32f365edb9326b52b33573e21a3482cdaabbae
Daniel Mazzeu <danmzzeu9@gmail.com>
2025-10-16 10:47:58 -0300

    Upload

--- javascript/ia_notifications.js
+++ javascript/ia_notifications.js
@@ -1,14 +1,14 @@
 import { createNotification } from './notifications.js';
 
 const messages = [
-    "Dica Dev: A melhor forma de aprender é começando um projeto novo. Que tal explorar um dos meus frameworks?",
-    "Que bom ter você por aqui! Dê uma olhada na minha expertise em Inteligência Artificial (FIAP).",
+    "Dica Dev: A melhor forma de aprender é começando um projeto novo.",
+    "Que bom ter você por aqui!.",
     "Não deixe de ver meus projetos em React! O desenvolvimento front-end é uma das minhas paixões.",
-    "Buscando um back-end robusto? Explore meu conhecimento em PHP e SQLServer!",
+    "Buscando um back-end robusto? Explore o conhecimento em PHP e SQLServer!",
     "Lembrete: O código limpo de hoje é o futuro fácil de amanhã. Continue explorando!",
-    "Parabéns por buscar inovação! Minha área de Python está ligada à Análise de Dados e IA.",
+    "Parabéns por buscar inovação!",
     "Curiosidade: Este portfólio é responsivo graças ao CSS-3, com foco em Mobile-First.",
-    "O que achou do design? Se precisar de algo do tipo, meu HTML e CSS são avaliados com 5 estrelas!",
+    "O que achou do design? Se precisar de algo do tipo, entre em contato comigo!",
     "Não esqueça de visitar meu projeto na L009, focado em automação de processos.",
     "Estou sempre inovando. Volte em breve para ver as novidades!",
 ];
